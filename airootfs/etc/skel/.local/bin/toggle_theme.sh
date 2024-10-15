c0a6e8780cc9d3bc98fee5a436c59793ec11dd12
hoream <hoream@qq.com>
2024-10-15 15:24:07 +0800

    update

--- airootfs/etc/skel/.local/bin/toggle_theme.sh
+++ airootfs/etc/skel/.local/bin/toggle_theme.sh
@@ -7,10 +7,12 @@ if [ "$curr" = "'default'" ]; then
     xfconf-query -c xfwm4 -p /general/theme -s "adw-gtk3-dark-small"
     xfconf-query -c xsettings -p /Net/ThemeName -s "adw-gtk3-dark"
     kvantummanager --set KvMojave
+    notify-send "主题切换" "切换为暗色主题\nQT应用需要重启"
 else
     gsettings set org.gnome.desktop.interface color-scheme default
     gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
     xfconf-query -c xfwm4 -p /general/theme -s "adw-gtk3-small"
     xfconf-query -c xsettings -p /Net/ThemeName -s "adw-gtk3"
     kvantummanager --set KvMojaveLight
+    notify-send "主题切换" "切换为亮色主题\nQT应用需要重启"
 fi
