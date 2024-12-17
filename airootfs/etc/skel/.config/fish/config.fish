d5797a474785660907966f8b0c42423918c935ca
hoream <hoream@qq.com>
2024-12-17 13:55:26 +0800

    fish enable vim mode

--- airootfs/etc/skel/.config/fish/config.fish
+++ airootfs/etc/skel/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     export MANPAGER='nvim +Man!'
+    set -g fish_key_bindings fish_vi_key_bindings
 
     alias ls="eza"
     alias ll='ls -alF'
